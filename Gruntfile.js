5bdbd8d004f2a641eaf0937ffc67d78af39864bd
vieron <javiersanchezmarin@gmail.com>
2013-09-12 00:23:06 +0200

    store in grunt config uick components defined in component.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    components: grunt.file.readJSON('component.json'),
     meta: {
       banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
               '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
