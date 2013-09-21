a3df61dae30b10bdbab94c9f8f3efc31fa90780e
vieron <javiersanchezmarin@gmail.com>
2013-09-21 23:10:54 +0200

    more tests

--- tests/spec/uick.js
+++ tests/spec/uick.js
@@ -10,8 +10,7 @@ describe('uick', function() {
     });
 
     afterEach(function() {
-        // this.uick.destroy();
-        delete this.uick;
+        this.uick.destroy();
     });
 
     it('should return uick instance', function() {
@@ -40,6 +39,7 @@ describe('uick', function() {
 
         beforeEach(function() {
             this.uick = uick('#el');
+            this.el = document.querySelector('#el');
 
             _.each(components, function(component) {
                 this.uick[component]();
@@ -47,41 +47,77 @@ describe('uick', function() {
         });
 
         afterEach(function() {
-            // this.uick.destroy();
-            delete this.uick;
+            this.uick.destroy();
         });
 
-        it('should return component instance when receives arguments: component name and HTMLelement', function() {
-            var el = document.querySelector('#el');
-            _.each(components, function(component) {
-                expect(this.uick.api(component, el)).to.be.an.instanceof(uick.components[component]);
-            }, this);
-        });
 
-        it('should return component instance when receives arguments: component name and index number', function() {
-            var el = document.querySelector('#el');
-            _.each(components, function(component) {
-                expect(this.uick.api(component, 0)).to.be.an.instanceof(uick.components[component]);
-            }, this);
-        });
-
-        it('should return an array of component instances when receives argument: component name', function() {
-            var el = document.querySelector('#el');
-            _.each(components, function(component) {
-                expect(this.uick.api(component)).to.have.length.above(0);
-                expect(this.uick.api(component)[0]).to.be.an.instanceof(uick.components[component]);
-            }, this);
-        });
-
-        it('should return an object of component instances when receives no arguments', function() {
-            var el = document.querySelector('#el');
+        it('api() should return an object of component instances when receives no arguments', function() {
             var api = this.uick.api();
-
             expect(api).to.be.a('object');
             _.each(components, function(component) {
                 expect(api).to.have.property(component);
             }, this);
         });
+
+
+        describe('#api(component_name, el)', function(){
+            it('should return component instance when receives arguments: component name and HTMLelement', function() {
+                _.each(components, function(component) {
+                    expect(this.uick.api(component, this.el)).to.be.an.instanceof(uick.components[component]);
+                }, this);
+            });
+        });
+
+        describe('#api(component_name, 0)', function(){
+            it('api(component_name, 0) should return component instance when receives arguments: component name and index number', function() {
+                _.each(components, function(component) {
+                    expect(this.uick.api(component, 0)).to.be.an.instanceof(uick.components[component]);
+                }, this);
+            });
+        });
+
+        describe('#api(component_name)', function(){
+            it('api(component_name) should return an array of component instances when receives argument: component name', function() {
+                _.each(components, function(component) {
+                    expect(this.uick.api(component)).to.have.length.above(0);
+                    expect(this.uick.api(component)[0]).to.be.an.instanceof(uick.components[component]);
+                }, this);
+            });
+        });
+    });
+
+
+
+
+
+    describe('#destroy()', function(){
+
+        beforeEach(function() {
+            this.uick = uick('#el');
+            this.el = document.querySelector('#el');
+
+            _.each(components, function(component) {
+                this.uick[component]();
+            }, this);
+        });
+
+        it('should call to destroy method of all components and delete all references to DOM elements and components\' instances', function() {
+            _.each(components, function(component) {
+                this.uick[component]();
+                expect(this.el.uick).to.be.a('object');
+                expect(this.el.uick[component]).to.be.an.instanceof(uick.components[component]);
+            }, this);
+
+            expect(this.uick.components).to.be.an('object');
+            expect(this.uick.components).to.contain.keys(components);
+
+            this.uick.destroy();
+
+            expect(this.uick.components).to.be.empty;
+            expect(this.el.uick).to.be.an('undefined');
+            expect(this.uick.el).to.be.empty;
+        });
+
     });
 
 });
